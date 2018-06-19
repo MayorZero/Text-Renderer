be336d558ed3f13766e53a6d83b7083df336319c
MayorZero <40370683+MayorZero@users.noreply.github.com>
2018-06-19 12:09:15 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -3,14 +3,14 @@
 
 Test::Test(Renderer2D* renderer2D)
 {
-  renderer = renderer2D;
+  m_renderer = renderer2D;
   
   FontManager::getInstance().Add(new Font("font", "font.ttf", 50);
   
-  label = new Label("Test", {0.0f, 0.0f}, {0.0f, 0.0f}, "font", 0xff00ffff);
+  m_label = new Label("Test", {0.0f, 0.0f}, {0.0f, 0.0f}, "font", 0xff00ffff);
 }
 
 void Test::Draw()
 {
-  label->Draw(renderer);
+  m_label->Draw(m_renderer);
 }
