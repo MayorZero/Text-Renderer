f8032ceff2cc44f8ec2782ff8a2bc93986886e63
MayorZero <40370683+MayorZero@users.noreply.github.com>
2018-06-19 12:01:48 +0200

    Update Texture.h

--- Texture.h
+++ Texture.h
@@ -4,7 +4,7 @@ public:
   Texture() = default;
   virtual ~Texture() = default;
 
-  Texture* Create(Uint width, Uint height);
+  Texture* Create(Uint width, Uint height) { return new GLTexture(width, height); }
   
   virtual void BindTexture(Uint slot = 0) {}
   virtual void UnbindTexture(Uint slot = 0) {}
@@ -16,4 +16,24 @@ public:
 protected:
   Uint w = 0, 
        h = 0;
+};
+
+class GLTexture : public Texture
+{
+public:
+  GLTexture(Uint width, Uint height);
+  ~GLTexture();
+
+  void BindTexture(Uint slot = 0) override;
+  void UnbindTexture(Uint slot = 0) override;
+
+  virtual void setData(const void* pixels) override;
+
+  void Free() override;
+
+private:		
+  std::string filePath;
+  Uint texture = 0;
+
+  Uint LoadFromFile();
 };
