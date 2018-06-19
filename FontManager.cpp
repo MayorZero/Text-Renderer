60b3719ece53e69764779a6a012c415731630a7b
MayorZero <40370683+MayorZero@users.noreply.github.com>
2018-06-19 11:56:36 +0200

    Create FontManager.cpp

---
+++ FontManager.cpp
@@ -0,0 +1,59 @@
+std::vector<std::unique_ptr<Font>> FontManager::font;
+
+	FontManager::~FontManager()
+	{
+		Free();
+	}
+
+	FontManager& FontManager::getInstance()
+	{
+		static FontManager instance;
+		return instance;
+	}
+
+	void FontManager::Add(Font* font)
+	{
+		this->font.push_back(std::unique_ptr<Font>(font));
+	}
+
+	Font* FontManager::Get()
+	{
+		return font[0].get();
+	}
+
+	Font* FontManager::Get(const std::string& name)
+	{
+		for (const auto& font : font) {
+			if (font->getName() == name) {
+				return font.get();
+			}
+		}
+		
+		return nullptr;
+	}
+
+	Font* FontManager::Get(MiUint size)
+	{
+		for (const auto& font : font) {
+			if (font->getFontSize() == size) {
+				return font.get();
+			}
+		}
+		
+		return nullptr;
+	}
+
+	Font* FontManager::Get(const std::string& name, MiUint size)
+	{
+		for (const auto& font : font) {
+			if (font->getFontSize() == size && font->getName() == name) {
+				return font.get();
+			}
+		}
+		return nullptr;
+	}
+	
+	void FontManager::Free()
+	{
+		font.clear();
+	}
