0f141434f2168e800a3a6395f104eedd25b3938f
MayorZero <40370683+MayorZero@users.noreply.github.com>
2018-06-19 12:15:34 +0200

    Update Label.h

--- Label.h
+++ Label.h
@@ -1,23 +1,16 @@
-class Label : public Sprite
-	{
-	public:
+class Label
+{
+public:
+	Label(const std::string& text, float x, float y, const std::string& font, Uint color);
 
-	public:
-		Label(const std::string& text, float x, float y, Uint color);
-		Label(const std::string& text, float x, float y, Font* font, Uint color);
-		Label(const std::string& text, float x, float y, const std::string& font, Uint color);
-		Label(const std::string& text, float x, float y, const std::string& font, Uint size, Uint color);
-    
-		void Draw(Renderer2D* renderer) const override;
-    
-		void ValidateFont(const std::string& name, int size = -1);
+	void Draw(Renderer2D* renderer) const;
 
-		void SetText(const std::string& text);
+	void SetText(const std::string& text);
 
-		inline const Font& getFont() const { return *m_font; }
-		inline const std::string& getText() const { return m_text; }
-	private:
-		void UpdateBounds();
+	inline const Font& getFont() const { return *m_font; }
+	inline const std::string& getText() const { return m_text; }
+private:
+	void UpdateBounds();
 
-		Font* m_font;
-		std::string m_text;
+	Font* m_font;
+	std::string m_text;
