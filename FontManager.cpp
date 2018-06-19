1f885045438c3c7084f43ae03909ab95f406a18b
MayorZero <40370683+MayorZero@users.noreply.github.com>
2018-06-19 11:57:28 +0200

    Update FontManager.cpp

--- FontManager.cpp
+++ FontManager.cpp
@@ -1,59 +1,59 @@
 std::vector<std::unique_ptr<Font>> FontManager::font;
 
-	FontManager::~FontManager()
-	{
-		Free();
-	}
+FontManager::~FontManager()
+{
+	Free();
+}
 
-	FontManager& FontManager::getInstance()
-	{
-		static FontManager instance;
-		return instance;
-	}
+FontManager& FontManager::getInstance()
+{
+	static FontManager instance;
+	return instance;
+}
 
-	void FontManager::Add(Font* font)
-	{
-		this->font.push_back(std::unique_ptr<Font>(font));
-	}
+void FontManager::Add(Font* font)
+{
+	this->font.push_back(std::unique_ptr<Font>(font));
+}
 
-	Font* FontManager::Get()
-	{
-		return font[0].get();
-	}
+Font* FontManager::Get()
+{
+	return font[0].get();
+}
 
-	Font* FontManager::Get(const std::string& name)
-	{
-		for (const auto& font : font) {
-			if (font->getName() == name) {
-				return font.get();
-			}
+Font* FontManager::Get(const std::string& name)
+{
+	for (const auto& font : font) {
+		if (font->getName() == name) {
+			return font.get();
 		}
-		
-		return nullptr;
 	}
 
-	Font* FontManager::Get(MiUint size)
-	{
-		for (const auto& font : font) {
-			if (font->getFontSize() == size) {
-				return font.get();
-			}
+	return nullptr;
+}
+
+Font* FontManager::Get(Uint size)
+{
+	for (const auto& font : font) {
+		if (font->getFontSize() == size) {
+			return font.get();
 		}
-		
-		return nullptr;
 	}
 
-	Font* FontManager::Get(const std::string& name, MiUint size)
-	{
-		for (const auto& font : font) {
-			if (font->getFontSize() == size && font->getName() == name) {
-				return font.get();
-			}
+	return nullptr;
+}
+
+Font* FontManager::Get(const std::string& name, Uint size)
+{
+	for (const auto& font : font) {
+		if (font->getFontSize() == size && font->getName() == name) {
+			return font.get();
 		}
-		return nullptr;
 	}
-	
-	void FontManager::Free()
-	{
-		font.clear();
-	}
+	return nullptr;
+}
+
+void FontManager::Free()
+{
+	font.clear();
+}
